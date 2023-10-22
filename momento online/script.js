f26f27df2cdcedd8613f1f0b7be045b7c8e3ae4f
Thayná Barbosa dos Santos <thaynasants@gmail.com>
2023-10-22 18:45:45 -0300

    locação de livros implementada com JS

--- momento online/script.js
+++ momento online/script.js
@@ -18,6 +18,22 @@ function enviar(){
     let texto = input.value;
 
     if(texto == "Sim"){
-        
+        document.getElementById("identificador_locacao").style.display = "block";
+        document.getElementById("conteudo").style.display = "none";
+
+        let livro = document.getElementById ("livro_select").options[document.getElementById("livro_select").selectedIndex].text;
+        document.getElementById('livro').innerHTML = "Livro: " + livro;
+        document.getElementById('prazo').innerHTML = "Prazo de devolução: 10 dias";
+        document.getElementById('num_inscricao').innerHTML = "Número de inscrição: " + document.getElementById('inscricao').value;
+    }
+
+    if(texto == "Não"){
+        document.getElementById("identificador_locacao").style.display = "block";
+        document.getElementById("conteudo").style.display = "none";
+
+        let livro = document.getElementById ("livro_select").options[document.getElementById("livro_select").selectedIndex].text;
+        document.getElementById('livro').innerHTML = "Livro: " + livro;
+        document.getElementById('prazo').innerHTML = "Prazo de devolução: 7 dias";
+        document.getElementById('num_inscricao').innerHTML = "Número de inscrição: " + document.getElementById('inscricao').value;
     }
 }
